3df5f6bbb2b8505209aa840467288599f9703a0a
codingbecca <131915898+codingbecca@users.noreply.github.com>
2025-02-21 08:26:28 -0800

    added accounts table

---
+++ app/(dashboard)/accounts/page.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle
+} from "@/components/ui/card"
+import { Loader2, Plus } from "lucide-react";
+import { columns } from "./columns";
+import { DataTable } from "@/components/data-table";
+import { Skeleton } from "@/components/ui/skeleton";
+
+
+const AccountsPage = () => {
+    const newAccount = useNewAccount();
+    const accountsQuery = useGetAccounts();
+    const accounts = accountsQuery.data || [];
+
+    if(accountsQuery.isLoading){
+        return (
+            <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+                <Card className="border-none drop-shadow-sm">
+                <CardHeader>
+                    <Skeleton className="h-8 w-48" />
+                </CardHeader>
+                <CardContent>
+                    <div className="h-[500px] w-full flex items-center justify-center">
+                        <Loader2 className="size-6 text-slate-300 animate-spin" />
+                    </div>
+                </CardContent>
+                </Card>
+            </div> 
+        );
+    }
+
+    return (
+        <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+            <Card className="border-none drop-shadow-sm">
+                <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+                    <CardTitle className="text-xl line-clamp-1">
+                        Accounts Page
+                    </CardTitle>
+                    <Button onClick={newAccount.onOpen} size='sm'>
+                        <Plus className="size-4 mr-2" />
+                        Add new
+                    </Button>
+                </CardHeader>
+                <CardContent>
+                <DataTable 
+                filterKey="name"
+                columns={columns} 
+                data={accounts}
+                onDelete={() => {}} />
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
+
+export default AccountsPage;
